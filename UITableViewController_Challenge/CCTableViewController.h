bf6e5cf74ebded46984a17a88d9bd2decad0bb7b
Arturo Lopez <arlopez@csumb.edu>
2014-09-02 04:14:52 -0700

    Completed challenge added sections

---
+++ UITableViewController_Challenge/CCTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  CCTableViewController.h
+//  UITableViewController_Challenge
+//
+//  Created by Arturo Lopez on 8/31/14.
+//  Copyright (c) 2014 Unsigned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@property (strong,nonatomic)NSString *user;
+@property (strong,nonatomic)UITableView *table;
+
+
+
+@end
